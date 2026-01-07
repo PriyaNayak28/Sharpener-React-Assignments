aa4874d99496a752d4a414f3942afdb38493285b
PriyaNayak28 <nayakpriya612@gmail.com>
2026-01-07 19:48:33 +0530

    first-custom-component-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
+
 import './App.css'
 import ExpenseItem from './components/Expenses'
 
 function App() {
-
   return (
     <>
-      <h1>Hello Let's Learn React!</h1>
+      <h1>let's get started</h1>
       <h2><ExpenseItem></ExpenseItem></h2>
     </>
   )
