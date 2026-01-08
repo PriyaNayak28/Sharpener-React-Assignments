5efc101d428f8005bc0166892a3799eb9b2201bc
PriyaNayak28 <nayakpriya612@gmail.com>
2026-01-08 21:39:14 +0530

    javascript-inside-jsx

--- src/component/ExpenseItem.jsx
+++ src/component/ExpenseItem.jsx
@@ -1,14 +1,18 @@
 import './ExpenseItem.css'
 
 function ExpenseItem () {
+    let expenseDate = new Date(2026 , 0 , 10);
+    let expenseLocation = 'Delhi';
+    let expenseTitle = 'book';
+    let expenseAmount = 10;
    return( 
    <>
     <div className='expense-item'>
-    <div>10/01/2026</div>
-    <div className='expense-item__location'>location - Delhi</div>
+    <div>{expenseDate.getDate()}</div>
+    <div className='expense-item__location'>{expenseLocation}</div>
     <div className='expense-item__description'>
-        <h2>book</h2>
-        <div className='expense-item__price'>$10</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
     </div>
     </div>
     </>
