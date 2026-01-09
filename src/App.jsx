6c96b4d41ab19bc71c9184eb3e0700340bc54a93
PriyaNayak28 <nayakpriya612@gmail.com>
2026-01-10 01:30:37 +0530

    Formatting Date - Giving Calendar Look

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import './App.css'
 function App() {
   let expenses = [  
     { id : 1,date : new Date(2026 ,1 ,6) , location : "banglore" , title : "insurence", amount : 20},
-    {id : 2,date : new Date(2026 ,2 ,5) , location : "indore" , title : "insurence", amount : 320},
-    {id : 3,date : new Date(2026 ,3 ,4) , location : "deelhi" , title : "insurence", amount : 200}
+    {id : 2,date : new Date(2025 ,2 ,5) , location : "indore" , title : "insurence", amount : 320},
+    {id : 3,date : new Date(2024 ,3 ,4) , location : "deelhi" , title : "insurence", amount : 200}
     ]
   
   return (
