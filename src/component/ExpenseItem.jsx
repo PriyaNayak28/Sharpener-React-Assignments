4097979a1ce1b6e1f25994f6867ea8023f681154
PriyaNayak28 <nayakpriya612@gmail.com>
2026-01-14 18:25:53 +0530

    splitting-the-components

--- src/component/ExpenseItem.jsx
+++ src/component/ExpenseItem.jsx
@@ -1,25 +1,19 @@
 import './ExpenseItem.css'
+import ExpenseDate  from './ExpenseDate'
+import Expenses from './Expenses'
 
 function ExpenseItem (props) {
-    let months = ["jan", "feb" , "march", "april" , "may" , "june" , "july" , "aug" , "sept" , "oct" , "nov" , "dec"];
-    let month = months[props.date.getMonth()];
-    let year = props.date.getFullYear();
-    let date = props.date.getDate();
-
+   
    return( 
    <>
     <div className='expense-item'>
-    {/* <div>{props.date.toISOString()}</div> */}
-    <div>
-    <div>{month}</div>
-    <div>{year}</div>
-    <div>{date}</div>
-    </div>
+     <ExpenseDate date = {props.date}></ExpenseDate>
     <div className='expense-item__location'>{props.location}</div>
     <div className='expense-item__description'>
         <h2>{props.Title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
     </div>
+   
     </div>
     </>
     )
